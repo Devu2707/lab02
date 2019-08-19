fab2f2ffbc3f322ece3da8273b4a3161a98aa7f4
user <devansh.pandey_cs18@gla.ac.in>
2019-08-19 14:29:03 +0530

    add input for variable:(userNae, String)

--- src/question01/Question01.java
+++ src/question01/Question01.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Question01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(system.in);
+        string userName = scanner.nextLine().trim();
         scanner.close();
     }
 }
